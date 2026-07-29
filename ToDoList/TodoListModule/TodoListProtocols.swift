9b892df6c9d46739ad3015a9d16d259f0148e8a5
kristinatsk <kristinatsk@gmail.com>
2026-07-29 23:14:19 +0300

    feat: initiate Todo list module

---
+++ ToDoList/TodoListModule/TodoListProtocols.swift
@@ -0,0 +1,22 @@
+import Foundation
+import UIKit
+
+protocol TodoListPresenterProtocol: AnyObject {
+    func viewDidLoad()
+}
+
+protocol TodoListViewProtocol: AnyObject {
+    func updateScreen(with todos: [ToDoEntity])
+}
+
+protocol TodoListInteractorProtocol: AnyObject {
+    func fetchTodos()
+}
+
+protocol TodoListInteractorOutputProtocol: AnyObject {
+    func didFetchTodos(_ todos: [ToDoEntity])
+}
+
+protocol TodoListRouterProtocol: AnyObject {
+    static func getModule() -> UIViewController
+}
